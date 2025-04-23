a97df44d5cefd219d515751160f4d78d074b9050
nosegit <134485645+nosegit@users.noreply.github.com>
2025-04-23 11:52:52 +0700

    Update ESP8266_Blynk.ts

--- ESP8266_Blynk.ts
+++ ESP8266_Blynk.ts
@@ -1,3 +1,17 @@
+/*
+This code is derived from:
+https://github.com/SolderedElectronics/pxt-wifi/tree/master
+
+MIT License
+
+Copyright 2019 E-radionica
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 enum HttpMethod {
     GET,
     POST,
@@ -20,7 +34,6 @@ namespace OKBit{
     /**
      * WiFi:bit commands.
      */
-    //% color=#3452C3 weight=90 icon="\uf1eb" block="BlynkMain"
     export namespace Blynk{
     function writeToSerial(data: string, waitTime: number): void {
         serial.writeString(data + "\u000D" + "\u000A")
